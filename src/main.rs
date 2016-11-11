0073d91a062cb742c5b84a097c21008af6e86481
tomcat1911 <maslovtimofey@gmail.com>
2016-11-11 20:23:46 +0500

    some datatypes and functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::collections::HashMap;
+
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType
+}
+
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData)
+}
+
+type AttrMap = HashMap<String, String>;
+
+fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data) }
+}
+
+fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unimplemented!()
 }
